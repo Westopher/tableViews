b0d03f7c5d432f967cdd4211b445bde764e05429
West Kraemer <WestKraemer@gmail.com>
2017-01-16 18:59:23 -0500

    add number of rows in table and defining cells with tableviewcell

--- tableViews/ViewController.swift
+++ tableViews/ViewController.swift
@@ -10,18 +10,19 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return 1
         
     }
     
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = UITableViewCell(style: UITableViewCell.default, reuseIdentifier: "Cell")
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
         
         cell.textLabel?.text = "Top row"
         
+        return cell
         
     }
 
