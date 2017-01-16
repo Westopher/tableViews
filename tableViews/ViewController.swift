1fb43d18b6d7533fa3fe27a1c9bd439ecc5542b6
West Kraemer <WestKraemer@gmail.com>
2017-01-16 18:02:42 -0500

    begin constructing table view in view controller and layout.

--- tableViews/ViewController.swift
+++ tableViews/ViewController.swift
@@ -8,8 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 1
+        
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCell.default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = "Top row"
+        
+        
+    }
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
